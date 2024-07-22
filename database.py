696aa6ead18454e5c253c795dbdd84b6d0d5fa4a
MARCOSTAKU <145709999+MARCOSTAKU@users.noreply.github.com>
2024-07-22 01:20:13 -0500

    Update database.py
    Conecxion actualizada

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import psycopg2
 
 def get_connection():
     conn = psycopg2.connect(
-        host="localhost",
+        host="dpg-cqevihggph6c73b260j0-a",
         port="5432",
-        user="postgres",
-        password="123",
-        dbname="Clientes-Web"
+        user="clientes_web_tcr4_user",
+        password="rHz9pXRBzFDMvo0e7S7XGWFvaGBf1ITM",
+        dbname="clientes_web_tcr4"
     )
     return conn
